fe2a1c036cfe0a551be8bd379bec25e9bbb303be
excelwang <wanghuajin999@163.com>
2019-04-26 10:09:12 +0800

    fix run-dev path

--- run-dev.sh
+++ run-dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export LC_ALL='en_US.utf8'
 cd pk1
-nohup python -u pk1/manage.py runserver 0:8080 2>&1 &
+nohup python -u manage.py runserver 0:8080 2>&1 &
 # nohup python -u manage.py monitor-cloud 2>&1 &
