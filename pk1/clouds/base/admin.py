bf0c37e8a53fd51a377b6badf4691ac982e8a406
excelwang <wanghuajin999@163.com>
2019-04-26 17:54:48 +0800

    use daa.admin.AutoModelAdmin

--- pk1/clouds/base/admin.py
+++ pk1/clouds/base/admin.py
@@ -2,95 +2,24 @@ from django.contrib import admin
 from django.db.models import Q, fields
 from django.utils.html import format_html
 from django.utils.timezone import now
+from daa.admin import AutoModelAdmin
 from clouds.utils import get_formated_url, get_url
 from user.models import Balance
 from .models import OPERATION_STATUS
 from django.db import transaction
 
-class AutoModelAdmin(admin.ModelAdmin):
-    def __init__(self, model, admin_site):
-        super().__init__(model,admin_site)
-        fs_head=[]
-        fs_tail=[]
-        fs_readonly=[]
-        fs_editable=[]
-        fs_list_display=[]
-        fs_all=self.model._meta.get_fields()
-        base_dict=self.model.__bases__[0].__dict__
-        for f in fs_all:
-            if f.auto_created or not f.concrete: continue
-            if not f.editable: fs_readonly.append(f)
-            if f.name not in base_dict: fs_head.append(f)
-            else: fs_tail.append(f)
-        self.head_fields=tuple(f.name for f in fs_head)
-        self.tail_fields=tuple(f.name for f in fs_tail)
-        for f in fs_head+fs_tail:
-            if f.editable: fs_editable.append(f)
-            if type(f) in (fields.TextField,fields.UUIDField,fields.related.ManyToManyField): continue
-            fs_list_display.append(f)
-        self.auto_readonly_fields=tuple(f.name for f in fs_readonly)
-        self.editable_fields=tuple(f.name for f in fs_editable)
-        self.auto_list_display=tuple(f.name for f in fs_list_display)
-    extra=()
-    form_fields_exclude=()
-    list_display_exclude=()
-    queryset_Q=None
-    form_field_queryset_Q=None
-    def get_extra(self, request, obj=None):
-        return self.extra
-    def get_form_fields_exclude(self, request, obj=None):
-        return self.form_fields_exclude
-    def get_list_display_exclude(self, request, obj=None):
-        return self.list_display_exclude
-    def get_queryset_Q(self, request):
-        return self.queryset_Q
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        if request.user.is_superuser: return qs
-        q=self.get_queryset_Q(request)
-        if q: qs = qs.filter(q)
-        return qs.distinct()
-    def get_fields(self, request, obj=None):
-        form_excls=self.get_form_fields_exclude(request,obj)
-        excls=self.get_exclude(request,obj)
-        if excls: form_excls+=excls
-        if self.fields: 
-            fs=fields
-        else:
-            fs=self.editable_fields
-            if obj: fs+=self.get_readonly_fields(request, obj)
-        return tuple(f for f in fs if f not in form_excls) if form_excls else fs
-    def get_readonly_fields(self, request, obj=None):
-        if not obj: return ()
-        if self.readonly_fields: return self.readonly_fields
-        return self.auto_readonly_fields+self.get_extra(request,obj)
-    def get_list_display(self, request, obj=None):
-        if self.list_display[0] != '__str__': return self.list_display
-        lde=self.get_list_display_exclude(request,obj)
-        fs=self.auto_list_display+self.get_extra(request,obj)
-        if lde: return tuple(f for f in fs if f not in lde)
-        return fs
-    def get_form_field_queryset_Q(self, db_field, request):
-        return self.form_field_queryset_Q
-    def _formfield_filter(self, db_field, request, call_back, **kwargs):
-        q=self.get_form_field_queryset_Q(db_field, request)
-        if q: kwargs["queryset"]=db_field.remote_field.model.objects.filter(q).distinct()
-        return call_back(db_field, request, **kwargs)
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        return self._formfield_filter(db_field, request, super().formfield_for_foreignkey, **kwargs)
-    def formfield_for_manytomany(self, db_field, request, **kwargs):
-        return self._formfield_filter(db_field, request, super().formfield_for_manytomany, **kwargs)
-    def get_form(self, request, obj=None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
-        for fname in request.GET:
-            if fname in form.base_fields:
-                # form.base_fields[fname].disabled=True
-                # form.base_fields[fname].widget=forms.HiddenInput()
-                form.base_fields[fname].widget.attrs={
-                    'readonly': True,
-                    'style': 'border: none; outline: none;'
-                }
-        return form
+def get_get_prefill_form(self, request, obj=None, **kwargs):
+    form = super().get_form(request, obj, **kwargs)
+    for fname in request.GET:
+        if fname in form.base_fields:
+            # form.base_fields[fname].disabled=True
+            # form.base_fields[fname].widget=forms.HiddenInput()
+            form.base_fields[fname].widget.attrs={
+                'readonly': True,
+                'style': 'border: none; outline: none;'
+            }
+    return form
+AutoModelAdmin.get_form=get_get_prefill_form
 
 #TODO use staticmodel of_user
 def powerful_form_field_queryset_Q(db_field, request):
