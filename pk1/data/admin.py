14c010b9637aaccab85c84fe136bf3b524a3ad35
excelwang <wanghuajin999@163.com>
2019-06-03 15:44:42 +0800

    add hue query url

--- pk1/data/admin.py
+++ pk1/data/admin.py
@@ -42,7 +42,8 @@ class DataInstanceAdmin(OwnershipModelAdmin,OperatableAdminMixin):
     form = DataInstanceForm
     def query(self, obj):
         # if not obj.ready: return None
-        return format_html('<a href="{}" target="_blank" class="button">Query</a>'.format('obj.uri_elected'))
+        url=obj.cluster.portal.split('//')[-1].split(':')[0]
+        return format_html('<a href="{}" type="{}" target="_blank" class="button">Query</a>'.format(url, obj.dataset.type_name))
     def action(self, obj):
         if obj.deleting:
             if not get_current_user().is_superuser: 
