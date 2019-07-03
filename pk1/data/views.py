d3c883236ee3c9d8accfbbaeb119bcc479569eeb
excelwang <wanghuajin999@163.com>
2019-07-03 13:53:21 +0800

    allow drf to access current opened space

--- pk1/data/views.py
+++ pk1/data/views.py
@@ -27,7 +27,6 @@ def space_state(request):
         # 'space':{}
     }
     space=get_space()
-    # print(space)
     state['cluster']['mem']={'total':38000,'used': 16000}
     state['cluster']['disk']={'total':15000,'used': 20000}
     # state['dataset']['total_type']=set([d.type_name for d in models.Dataset.objects.all()])
