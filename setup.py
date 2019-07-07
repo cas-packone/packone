ebd2bcf85076e51e3b8f7c0495a78cac66c5b4da
excelwang <wanghuajin999@163.com>
2019-07-07 16:11:27 +0800

    bump pip version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "pk1",
-    version = "0.0.7",
+    version = "0.1.0-beta.1",
     keywords = ("pip", "packone"),
     description = "PackOne: Pack clouds, engines and data services into one light stack.",
     long_description = open('README.rst').read(),
