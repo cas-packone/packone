bd87e8c2b1cda85f0e22c1fd2e9c9aa6aa149dee
root <excelwang>
2019-08-07 09:03:05 +0800

    extending h3cloudos request timeout

--- pk1/clouds/drivers/h3cloudos.py
+++ pk1/clouds/drivers/h3cloudos.py
@@ -43,20 +43,20 @@ class Driver(object):
         self.flavors=FlavorManager(self)
         self.keypairs=KeyManager()
     def _request(self,url,method=requests.get,data=None,retry_when_response_unexpected_strings=None,retry_until_response_expected_strings=None):
-        mustend = time.time() + 600
+        mustend = time.time() + 900
         while time.time() < mustend:
             res = method(self.endpoint+url,headers=self._headers,data=json.dumps(data))
             self.log.info('REQUEST: token/{}, {} {}, status/{}'.format(self._token, method.__name__, url, res.status_code))
             if retry_when_response_unexpected_strings:
                 ss=list(filter(lambda x: x in res.text, retry_when_response_unexpected_strings))
                 if ss:
-                    self.log.info('REQUEST RETRING, AS UNEXPECTED RESPONSE STRING "{}" is appeared in response: "{}"!'.format(ss[0], res.text))
+                    self.log.info('REQUEST RETRING, AS UNEXPECTED RESPONSE STRING "{}" appeared in response: "{}"!'.format(ss[0], res.text))
                     time.sleep(5)
                     continue
             if retry_until_response_expected_strings:
                 ss=list(filter(lambda x: x in res.text, retry_until_response_expected_strings))
                 if not ss:
-                    self.log.info('REQUEST RETRING, AS EXPECTED RESPONSE STRING "{}" is not appeared in response: "{}"!'.format(' or '.join(retry_until_response_expected_strings), res.text))
+                    self.log.info('REQUEST RETRING, AS EXPECTED RESPONSE STRING "{}"  didn\'t appear in response: "{}"!'.format(' or '.join(retry_until_response_expected_strings), res.text))
                     time.sleep(5)
                     continue
             break
