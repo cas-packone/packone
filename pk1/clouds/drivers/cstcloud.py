3992c9db8e379a28991452ead7abd56264d97fe9
root <excelwang>
2019-08-07 15:55:44 +0800

    opt cstcloud credential input

--- pk1/clouds/drivers/cstcloud.py
+++ pk1/clouds/drivers/cstcloud.py
@@ -3,9 +3,10 @@ from .h3cloudos import ImageManager as H3CImageManager
 from .h3cloudos import Image
 from .h3cloudos import InstanceManager#TODO  for InstanceManager.mountable_status
 
-
 class Driver(H3CDriver):
     def __init__(self, cloud):
+        if not 'nova-availability_zone' in cloud.platform_credential: cloud.platform_credential['nova-availability_zone']="CASCloud01"
+        if not 'cinder-availability_zone' in cloud.platform_credential: cloud.platform_credential['cinder-availability_zone']="cstcloud_cinder01"
         cloud.platform_credential['endpoint']='http://159.226.245.2:9000'
         H3CDriver.__init__(self, cloud)
         self.images=ImageManager(self)
