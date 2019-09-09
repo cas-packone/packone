348b650c047a57281fabd651da54a29f6e06be7d
root <excelwang>
2019-09-09 10:40:34 +0800

    fix django version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "pk1",
-    version = "0.2.4",
+    version = "0.2.5",
     keywords = ("pip", "packone"),
     description = "PackOne: Pack clouds, engines and data services into one light stack.",
     long_description = open('README.rst').read(),
@@ -16,7 +16,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    install_requires = ['django>=2.3','uwsgi','ambari','django-admin-auto','djangorestframework','djangorestframework-simplejwt','django-simpleui', 'django-filter', 'psycopg2-binary', 'coreapi', 'paramiko', 'scp', 'django-cors-headers==2.4.0', 'python-novaclient', 'python-cinderclient', 'python-glanceclient', 'django-autocomplete-light'],
+    install_requires = ['django','uwsgi','ambari','django-admin-auto','djangorestframework','djangorestframework-simplejwt','django-simpleui', 'django-filter', 'psycopg2-binary', 'coreapi', 'paramiko', 'scp', 'django-cors-headers==2.4.0', 'python-novaclient', 'python-cinderclient', 'python-glanceclient', 'django-autocomplete-light'],
     entry_points = {
         'console_scripts': [
             'pk1 = pk1.server:main',
