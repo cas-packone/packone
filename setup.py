c52a04ff8e682cd86eccfe28681a6e1f6915d676
excelwang <wanghuajin999@163.com>
2022-11-12 19:32:02 +0800

    pin versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "pk1",
-    version = "0.3.0",
+    version = "0.3.1",
     keywords = ("pip", "packone"),
     description = "PackOne: Pack clouds, engines and data services into one light stack.",
     long_description = open('README.rst').read(),
@@ -16,7 +16,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    install_requires = ['django','uwsgi','ambari','django-admin-auto','djangorestframework','djangorestframework-simplejwt','django-simpleui', 'django-filter', 'psycopg2-binary', 'coreapi', 'paramiko', 'scp', 'django-cors-headers==2.4.0', 'python-novaclient', 'python-cinderclient', 'python-glanceclient', 'django-autocomplete-light'],
+    install_requires = ['django==2.2.28','uwsgi==2.0.21','ambari==0.1.7','django-admin-auto==0.1.1','djangorestframework==3.10.3','djangorestframework-simplejwt==4.3.0','django-simpleui==4.0.3', 'django-filter==2.4.0', 'psycopg2-binary==2.8.6', 'coreapi==2.3.3', 'paramiko==2.7.2', 'scp==0.14.4', 'django-cors-headers==2.4.0', 'python-novaclient==17.7.0', 'python-cinderclient==7.4.1', 'python-glanceclient==3.6.0', 'django-autocomplete-light==3.8.2'],
     entry_points = {
         'console_scripts': [
             'pk1 = pk1.server:main',
